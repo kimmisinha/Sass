2847c0c2690c2d15a827ac44d3894cb38ff65dcb
kimmisinha <kimmi.sinha@newput.com>
2024-09-26 12:52:05 +0530

    Added Sass string function manipulations

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import "./styles/app.scss";
 function App() {
   return (
     <>
-   <div className="productcard">
-    <h2>sample</h2>
-    <button>button click !</button>
-    <p className="p1">Click me</p>
-    <p className="p2">Click me</p>
-    <p className="p3">Click me</p>
-   </div>
+      <div className="productcard">
+        <h2>sample</h2>
+        <button>button click !</button>
+        <p className="p1">Click me</p>
+        <p className="p2">Click me</p>
+        <p className="p3">Click me</p>
+      </div>
+      <div>
+        <p className="quoted-text">Hi</p>
+      </div>
     </>
   );
 }
